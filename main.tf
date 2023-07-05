a6f433c64b508a1db826de28526bb5aac0055342
unknown <austinkennedy@students.uonbi.ac.ke>
2023-07-05 16:18:24 +0300

    used interpolation and var.serverport for port

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-2"
 
 }
+variable "server_port" {
+  description = "this is the server port variable to avoid DRY"
+  type        = number
+  default     = 8080
+}
 resource "aws_instance" "myfirsttf" {
   ami                         = "ami-0fb653ca2d3203ac1"
   instance_type               = "t2.micro"
@@ -9,7 +14,7 @@ resource "aws_instance" "myfirsttf" {
   user_data                   = <<-EOF
                 #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
   user_data_replace_on_change = true
   tags = {
@@ -20,8 +25,8 @@ resource "aws_instance" "myfirsttf" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
